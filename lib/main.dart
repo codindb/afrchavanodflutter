2a4eb49f596b83ed4c3b16586f5fb7cad9b8ccf4
codindb <codindb.dev@gmail.com>
2021-04-16 17:59:00 +0200

    First implementations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:afrchavanodflutter/widgets/AfrAppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'layout/adaptive.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AFR Chavanod',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -21,14 +26,17 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -39,28 +47,15 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
+    final isDesktop = isDisplayDesktop(context);
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -68,46 +63,29 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+       body: CustomScrollView(
+         slivers: [
+           AfrAppBar(),
+           SliverList(
+             delegate: SliverChildListDelegate(
+               [
+                 Column(
+                   mainAxisAlignment: MainAxisAlignment.start,
+                   children: [
+                    Container(
+                      height: 1000,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ],
+                 )
+               ]
+             )
+           ),
+         ],
+       ),
     );
   }
 }
---
+++ lib/widgets/AfrAppBar.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../layout/adaptive.dart';
+
+class AfrAppBar extends StatefulWidget {
+  @override
+  _AfrAppBarState createState() => _AfrAppBarState();
+}
+
+class _AfrAppBarState extends State<AfrAppBar> with SingleTickerProviderStateMixin {
+  AnimationController _menuIconAnimationController;
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _menuIconAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _menuIconAnimationController.dispose();
+  }
+
+  void _menuIconToggle() {
+    setState(() {
+      isPlaying = !isPlaying;
+      isPlaying ? _menuIconAnimationController.forward() : _menuIconAnimationController.reverse();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDesktop = isDisplayDesktop(context);
+    return SliverAppBar(
+      iconTheme: IconThemeData(
+        color: Colors.black26,
+      ),
+      pinned: true,
+      backgroundColor: Colors.white,
+      expandedHeight: isDesktop ? 300.0 : 160.0,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Image.asset(
+          "assets/images/afrLogo.png",
+          width: isDesktop ? 200.0 : 80.0,
+        ),
+        background: Image.asset(
+          "assets/images/appBarBackground.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
+      actions: [
+        IconButton(
+          // color: Colors.black26,
+          iconSize: 50,
+          icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_close,
+            progress: _menuIconAnimationController,
+            semanticLabel: 'Show menu',
+          ),
+          onPressed: () {
+            _menuIconToggle();
+            showGeneralDialog(
+              barrierLabel: "Barrier",
+              barrierDismissible: true,
+              barrierColor: Colors.black.withOpacity(0.3),
+              transitionDuration: Duration(milliseconds: 500),
+              context: context,
+              pageBuilder: (context, anim1, anim2) {
+                return Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    height: 300,
+                    margin: EdgeInsets.only(top: 55, left: 12, right: 12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    // width: MediaQuery.of(context).size.width * 1,
+                    child: SizedBox.expand(
+                      child: FlutterLogo()
+                    ),
+                  ),
+                );
+              },
+              transitionBuilder: (context, animation, anim2, child) {
+                return SlideTransition(
+                  position: Tween(begin: Offset(1, 0), end: Offset(0, 0))
+                      .animate(animation),
+                  child: child,
+                );
+                // return FadeTransition(opacity: animation, child: child,);
+                // return ScaleTransition(
+                //   scale: animation,
+                //   child: child,
+                // );
+              },
+            ).then((returnVal) {
+              _menuIconToggle();
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
